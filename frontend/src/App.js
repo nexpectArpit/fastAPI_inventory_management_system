c53bfa6117ef5de0b9c223c55017a7eb2a182fc3
ARPIT TRIPATHI <arpittripathiayo@gmail.com>
2025-11-30 20:51:53 +0530

    Change API base URL to production server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+    baseURL: "https://fastapi-inventory-management-system.onrender.com",
 });
 
 function App() {
